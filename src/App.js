5b5029c10f885fbdb839ceef4f2b1ace57e4179c
pj <pj@dev.home>
2018-04-04 08:31:15 +0100

    Added ability to search any string

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 // Setting up API requests
-const DEFAULT_QUERY = 'redux';
+const DEFAULT_QUERY = 'hi';
 
 const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
@@ -11,40 +11,6 @@ const PARAM_SEARCH = 'query=';
 
 // http://hn.algolia.com/api/v1/search?query=react
 
-
-const list = [
-  {
-    title: "React",
-    url: "www.google.com",
-    author: "Prajay",
-    num_comments: 3,
-    points: 4,
-    objectID: 0
-  },
-  {
-    title: "Redux",
-    url: "www.apple.com",
-    author: "Te homies",
-    num_comments: 2,
-    points: 4,
-    objectID: 1
-  },
-  {
-    title: "Vue",
-    url: "www.apple.com",
-    author: "Some lads",
-    num_comments: 3,
-    points: 6,
-    objectID: 2
-  }
-];
-
-const stats = [
-  {
-    CommentNum:
-      list[0].num_comments + list[1].num_comments + list[2].num_comments
-  }
-];
 
 //function isSearch(searchedTerm) {
 //  return function(item) {
@@ -82,8 +48,8 @@ class App extends Component {
     this.setState({ result });
   }
 
-  // fetchSearchTopStories(searchTerm) {
-  //   fetch('http://hn.algolia.com/api/v1/search?query=react')
+  // setSearchTopStories(searchTerm) {
+  //   fetch('http://hn.algolia.com/api/v1/search?query=' + searchTerm)
   //     .then(response => (response.json()))
   //     .then(result => this.setSearchTopStories(result))
   //     .catch(e => console.log(e))
@@ -91,14 +57,26 @@ class App extends Component {
 
   componentDidMount() {
     const { searchTerm } = this.state;
-      fetch('http://hn.algolia.com/api/v1/search?query=react')
-      .then(response => (response.json()))
+
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+      .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
-      .catch(e => console.log(e))
+      .catch(error => error);
   }
 
+
+
+
+
   onSearchChange(event) {
+    // 
     this.setState({ searchTerm: event.target.value });
+    if (event.target.value.length > 0) {
+      fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${event.target.value}`)
+        .then(response => response.json())
+        .then(result => this.setSearchTopStories(result))
+        .catch(error => error); // Find a way to better catch an error. Show no results on the table. 
+    }
     // When using a handler in the element you get access to the synthetic React event
     // in your callback function's signature. So on the field changing the blank string
     // we defined as searchTerm is now updated to whatever the user has entered.
@@ -110,7 +88,7 @@ class App extends Component {
     const updatedList = this.state.list.filter(isNotId);
     // Here the isNotId function is laid out using ES6 notation, it checks that the object
     // ID isn't the same as the one given in the button click. The filter function will
-    // then give a list of items that aren't the one clicked the
+    // then give a list of items that aren't the one clicked
     this.setState({ list: updatedList });
     // Now we update the state of the component, what's happening here is the setState
     // function is updating the list in the internal component state
@@ -119,21 +97,21 @@ class App extends Component {
   render() {
     const { searchTerm, result } = this.state;
 
-    if (!result) { return null }
+    if (!result) { return null } // Add logic here to show what happens when nothing is found. 
     return (
-      
+
       <div className="page">
         <div className="interactions">
           <Search value={searchTerm} onChange={this.onSearchChange}>
             Search -
           </Search>
 
-          <Table list={result.hits} pattern={searchTerm} onDismiss={this.onDismiss} />
-        
+          <Table list={result.hits } pattern={searchTerm} onDismiss={this.onDismiss} />
+
 
           {/* <Summary list={result} stats={stats} /> */}
         </div>
-        
+
       </div>
     );
   }
@@ -147,24 +125,31 @@ class Search extends Component {
         <h1> Search Function </h1>
         <form>
           {children}
-          
+
           <input type="text" value={value} onChange={onChange} />
+          <Button
+            onClick="{() => onDismiss(item.objectID)}"
+            type="button"
+            className="button-inline"
+          > Search </Button>
         </form>
       </div>
     );
   }
 }
 
-class Summary extends Component {
-  render() {
-    const { list, stats } = this.props;
-    return (
-      <div className="Stats">
-        <h1> {stats[0].CommentNum.toString()} </h1>
-      </div>
-    );
-  }
-}
+// class Summary extends Component {
+//   render() {
+//     const { list, stats } = this.props;
+//     return (
+//       <div className="Stats">
+//         <h1> {stats[0].CommentNum.toString()} </h1>
+//       </div>
+//     );
+//   }
+// }
+// Old component used to fetch item stats. No longer used with API.
+
 
 class Table extends Component {
   render() {
